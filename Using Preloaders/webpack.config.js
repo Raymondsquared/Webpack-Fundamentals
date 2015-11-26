43fae3be68d00dca2c23ac68e55a9fec0c4216c6
Raymond.Raymond <Raymond.Raymond@carsales.com.au>
2015-11-26 12:21:25 +1100

    Using Preloaders

---
+++ Using Preloaders/webpack.config.js
@@ -0,0 +1,39 @@
+module.exports =
+{
+	entry: 	
+	[
+		"./utils",
+		"./app.js"
+	],
+	output:
+	{
+		filename: "bundle.js"
+	},
+	module:
+	{
+		preLoaders:
+		[
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'jshint-loader'
+			}
+		],
+		loaders:
+		[
+			{
+				test: /\.es6$/,
+				exclude: /node_modules/,
+				loader: 'babel',
+				query: 
+				{
+			    	presets: ['react', 'es2015']
+			    }
+			}
+		]
+	},
+	resolve:
+	{
+		extensions: ['', '.js', '.es6']
+	}
+}
