390d861b6ece7a2b7b2912003534c0e770282b2c
Raymond.Raymond <Raymond.Raymond@carsales.com.au>
2015-12-01 09:49:09 +1100

    Using SCSS And SASS

---
+++ Adding CSS to Your Build/Using SCSS And SASS/webpack.config.js
@@ -0,0 +1,54 @@
+var path = require('path');
+
+module.exports =
+{
+	context: path.resolve('js'),
+	entry: 	
+	[		
+		"./app"
+	],
+	output:
+	{
+		path: path.resolve('build/js/'),
+		publicPath: '/public/assets/js/',
+		filename: "bundle.js"
+	},
+	devServer:
+	{
+		contentBase: 'public'
+	},
+	module:
+	{
+		preLoaders:
+		[
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'jshint-loader'
+			}
+		],
+		loaders:
+		[
+			{
+				test: /\.es6$/,
+				exclude: /node_modules/,
+				loader: 'babel',
+				query: 
+				{
+			    	presets: ['react', 'es2015']
+			    }
+			},
+			{
+				test: /\.css$/,
+				exclude: /node_modules/,
+				loader: 'style!css',
+			},
+			{
+				test: /\.scss$/,
+				exclude: /node_modules/,
+				loader: 'style!css!sass',
+			}
+		]
+	},
+	watch: true
+}
